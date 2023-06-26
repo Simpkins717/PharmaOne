38e2297e79ed0a0074c307473f1566714577a9e7
Simpkins717 <dsimpkins717@gmail.com>
2023-06-26 14:05:58 -0400

    building footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,27 @@
+import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
 const Footer = () => {
   return (
-    <div className='bg-cyan-500 absolute bottom-0 min-w-full h-20'>Footer</div>
+    <div className='bg-cyan-400 absolute bottom-0 min-w-full '>
+      <div>
+        <h1>Company</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero hic
+          velit, aliquam temporibus necessitatibus dolores
+        </p>
+      </div>
+      <div>
+        <h1>Support</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          minima delectus blanditiis, culpa saepe aliquid nobis velit a?
+        </p>
+      </div>
+      <div>
+        <AiFillTwitterCircle />
+        <AiFillLinkedin />
+        <h1>PharmaOne &copy;</h1>
+      </div>
+    </div>
   );
 };
 
